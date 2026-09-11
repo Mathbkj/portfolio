1f590371e13919ec52396119890bf3d1a8a6880e
Mathbkj <matheusgblasel@hotmail.com>
2026-09-11 14:07:12 -0300

    remove react three fiber

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
 import { ReactLenis } from 'lenis/react'
 import 'lenis/dist/lenis.css'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,9 @@
-import { Canvas } from '@react-three/fiber'
-import { Environment, OrbitControls } from '@react-three/drei'
-import { Suspense } from 'react'
-import RobotModel from './RobotModel'
+import heroImg from '../assets/hero.png'
 
 function Hero() {
   return (
     <section className="flex min-h-[80vh] w-full flex-col items-center justify-center px-6 py-16">
-      <div className="h-[80vh] w-full">
-        <Canvas camera={{ position: [0, 1, 2], fov: 45 }}>
-          <ambientLight intensity={0.6} />
-          <directionalLight position={[3, 3, 3]} intensity={1} />
-          <Suspense fallback={null}>
-            <RobotModel scale={1} position={[0, -1, 0]} rotation={[0, Math.PI / 2, 0]} />
-            <Environment preset="city" />
-          </Suspense>
-          <OrbitControls enableZoom={false} />
-        </Canvas>
-      </div>
+      <img src={heroImg} alt="" className="h-[80vh] w-full object-contain" />
     </section>
   )
 }
